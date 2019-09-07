9b14e716e5ec6bdfa9e1884ababb22b220da1fd7
mridulganga <mridul.kepler@gmail.com>
2019-09-07 14:51:55 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: Free, open source, & modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a
+            :class="{'navbar-item': true, 'is-active is-tab':current_page=='events'}"
+            @click="change_page('events')"
+          >Events</a>
+          <a
+            :class="{'navbar-item': true, 'is-active is-tab':current_page=='members'}"
+            @click="change_page('members')"
+          >Members</a>
+          <a
+            :class="{'navbar-item': true, 'is-active is-tab':current_page=='announcements'}"
+            @click="change_page('announcements')"
+          >Announcements</a>
+        </div>
+      </div>
+    </nav>
+    <div class="main-container">
+      <events v-show="current_page=='events'" :events_list="events" @reloadEvents="loadEvents"></events>
+      <members v-show="current_page=='members'" :members_list="members" @reloadMembers="loadMembers"></members>
+      <announcements v-show="current_page=='announcements'"></announcements>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Events from "./pages/events.vue";
+import Members from "./pages/members.vue";
+import Announcements from "./pages/announcements.vue";
+import axios from 'axios';
+
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Events,
+    Members,
+    Announcements
+  },
+  data() {
+    return {
+      current_page: "events",
+      events:[],
+      members:[]
+    };
+  },
+  methods: {
+    change_page(page) {
+      this.current_page = page;
+      if (page=='events'){this.loadEvents()}
+      else if (page=='members'){this.loadMembers()}
+    },
+    alert(m) {
+      alert(m);
+    },
+    loadEvents(){
+      axios.get("https://nisb-events.herokuapp.com/events").then(respose => {
+        this.events = respose.data
+      })
+    },
+    loadMembers(){
+      axios.get("https://nisb-members.herokuapp.com/members?auth=all").then(respose => {
+        this.members = respose.data
+      })
+    }
+  },
+  created(){
+    this.loadEvents()
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-container {
+  padding: 20px;
+}
+html,
+body {
+  position: absolute;
+  background: #eee;
+  min-height: 100% !important;
+  width: 100%;
 }
 </style>
---
+++ src/pages/members.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="members">
+    Members Page
+    {{members_list}}
+    <b-table :data="members_list" :columns="columns"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Members',
+  props: ["members_list"],
+  data(){
+    return{
+      columns:[
+        {"field":"name","label":"Name"},
+        {"field":"email","label":"Email"},
+        {"field":"mobile","label":"Mobile"},
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
